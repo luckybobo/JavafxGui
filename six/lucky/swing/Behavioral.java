68cbc006cf28c3993a193ed302dc67fdd78988ca
luckybob <121589971+luckybobo@users.noreply.github.com>
2023-02-05 14:07:52 +0800

    Delete six/lucky directory

--- six/lucky/swing/Behavioral.java
+++
@@ -1,5 +0,0 @@
-package six.lucky.swing;
-
-public interface Behavioral {
-    void personaBehavioral(int frequency,boolean isEven);
-}
--- six/lucky/swing/KeyPressed.java
+++
@@ -1,5 +0,0 @@
-package six.lucky.swing;
-
-public interface KeyPressed extends KeyEvent {
-    void personaKeyPressedEvent(javafx.scene.input.KeyEvent event);
-}
